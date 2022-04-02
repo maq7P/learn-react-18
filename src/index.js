f03c084117617a1d633619fa6e4d95efeb98a818
maq7 <maxpet16@mail.ru>
2022-04-02 20:08:24 +0500

    initialization react 18

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+// React 17
+// ReactDOM.render(<App />, document.getElementById('root'))
+
+reportWebVitals();
